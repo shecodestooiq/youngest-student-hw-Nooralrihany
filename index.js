92125593d5f1bd25523c450a2ddcd47784a4e3f6
Nooralrihany <Nooralrihany@gmail.com>
2024-05-13 18:31:44 +0300

    first commite

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function showYoungestStudent(students) { }
+function showYoungestStudent(students) { 
+}
 
 module.exports = showYoungestStudent;
+
+var students = [
+    {name : "noor" ,age: 23} ,
+    {name : "aya" , age:19}, 
+    {name : "ahmed" , age:33},
+];
+
+var max = Math.max( [    {name : "noor" ,age: 23} ,
+{name : "ahmed" , age:33},])
+var maxValues  = students.filter (i => i == max)
+
+var min = math.min ([{name : "aya" , age:19},])
+var minValues = students.filter (i => i == min)
+
+console.log(maxValues)
+console.log(minValues)
